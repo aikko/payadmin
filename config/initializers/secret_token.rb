042c0b9c7b9cfd9a2b9ccbc25e3777705f03629f
aikko <aikko.woo@gmail.com>
2013-03-13 09:44:25 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Payadmin::Application.config.secret_token = '006db0513af61e227e67fcfe7e8ac51f62254b49f8cb23935b14ffff32e141148da980479f364e4bd7b3cff0934c91fc6ea75f76ee81a40acf358a917ec66bec'
